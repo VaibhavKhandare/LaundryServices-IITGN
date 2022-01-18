9d6127cef36f7c3d0d254b73199346f2088c6c96
unknown <lpsk5713@gmail.com>
2022-01-19 03:34:48 +0530

    Started Project

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+// To generate SHA
+// Install JDK and set keytool environment variable
+// then keytool -list -v -keystore "C:\Users\yourname\.android\debug.keystore" -alias androiddebugkey -storepass android -keypass android
+// Google Sign in - https://www.youtube.com/watch?v=1k-gITZA9CI
+import 'package:flutter/material.dart';
